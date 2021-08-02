f3e50829aeb0c3b67a146a1e849877846bc066ed
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-08-02 18:42:43 -0500

    Set up dotenv, imdb api calls, parsed JSON movie data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
+require('dotenv').config()
+
 var express = require('express')
 var router = express.Router()
+const request = require('request')
+
+const apiKey = process.env.API_KEY
+const apiBaseUrl = 'http://api.themoviedb.org/3'
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300'
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {})
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    // console.log('==== The error ====')
+    // console.log(error)
+    // console.log('==== The response ====')
+    // console.log(response) // where all the status codes, headers, etc are at
+    // console.log('==== The movieData ====')
+    // console.log(movieData) // it's just the body in string format
+    const parsedData = JSON.parse(movieData) // we get a JS object that we can work with
+    // console.log(parsedData)
+    res.json(parsedData)
+  })
 })
 
 module.exports = router
